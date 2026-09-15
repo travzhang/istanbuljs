70a4f3417fc231fe795438b62def6aeb3a5da1af
Travis Zhang <wr.zhang25@gmail.com>
2026-09-15 11:26:13 +0800

    chore: add package fileTags for html-modern coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,7 +9,22 @@ export default defineConfig({
       enabled: true,
       provider: "v8",
       include: ["packages/**/src/**/*.ts"],
-      reporter: ["json", ["html-modern", { writeReportDataJson: true }]],
+      reporter: [
+        "json",
+        [
+          "html-modern",
+          {
+            writeReportDataJson: true,
+            fileTags: [
+              { glob: "packages/istanbul-lib-coverage/**", tag: "lib-coverage" },
+              { glob: "packages/istanbul-lib-instrument/**", tag: "lib-instrument" },
+              { glob: "packages/istanbul-lib-report/**", tag: "lib-report" },
+              { glob: "packages/istanbul-lib-source-maps/**", tag: "lib-source-maps" },
+              { glob: "packages/istanbul-report-html-modern/**", tag: "html-modern" },
+            ],
+          },
+        ],
+      ],
     },
     projects: ["packages/*/vitest.config.*"],
   },
